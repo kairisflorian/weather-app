89dc93a44bbe4117b8466314de0f0e58d3024a6b
kairisflorian <kairisflorian@gmail.com>
2024-03-20 21:42:17 +0100

    travail sur le design de la navigation de l'app

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,7 @@ export default {
         extend: {
             colors: {
                 "weather-primary": "#00668A",
-                "weather-secondary": "004E71",
+                "weather-secondary": "#004E71",
             },
         },
         fontFamily: {
